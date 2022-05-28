bbd121a2f4ad684870689b01dfc6570cdecb8e27
Areeb Malik <areeb.web.intern@gmail.com>
2022-05-29 00:55:05 +0500

    Added a new property in gulp file for purging unused css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-// these are the imports we need for gulp and sass
+// these are the imports we need for gulp and sass and the purgecss for removing unused css
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 
 
@@ -12,11 +13,13 @@ dest will be used to speficy our output css file
 function buildStyles() {
     /**
      * this means that look inside the named folder and then all sub-folders
-     * and compile all the sass files even if they are inside the sub-folders
+     * and compile all the sass files even if they are inside the sub-folders.
+     * This will also purge(remove) any css that we have not used
      */
 
     return src('basics/**/*.scss')
     .pipe(sass())
+    .pipe(purgecss({content: ['*.html']}))
     .pipe(dest('css'))
 }
 
@@ -24,7 +27,7 @@ function buildStyles() {
  * A watch function 
 */
 function watchTask() {
-    watch(['basics/**/*.scss'], buildStyles)
+    watch(['basics/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
